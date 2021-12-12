1ca2b5d9bcf55805a636e24be95752dae184e7e6
TOOSSA <81390547+TOUAOUSSA-Oussama@users.noreply.github.com>
2021-12-12 12:15:30 +0100

    init

---
+++ snake_game_envirement.py
@@ -0,0 +1,157 @@
+import pygame
+import random
+from enum import Enum
+from collections import namedtuple
+import numpy as np
+
+pygame.init()
+font = pygame.font.Font('arial.ttf', 25)
+#font = pygame.font.SysFont('arial', 25)
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+    
+Point = namedtuple('Point', 'x, y')
+
+# rgb colors
+WHITE = (255, 255, 255)
+RED = (200,0,0)
+BLUE1 = (0, 0, 255)
+BLUE2 = (0, 100, 255)
+BLACK = (0,0,0)
+
+BLOCK_SIZE = 20
+SPEED = 20
+
+class SnakeGameAI:
+    
+    def __init__(self, w=640, h=480):
+        self.w = w
+        self.h = h
+        # init display
+        self.display = pygame.display.set_mode((self.w, self.h))
+        pygame.display.set_caption('Snake')
+        self.clock = pygame.time.Clock()
+        self.reset()
+        
+
+    def reset(self):
+        # init game state
+        self.direction = Direction.RIGHT
+        
+        self.head = Point(self.w/2, self.h/2)
+        self.snake = [self.head, 
+                      Point(self.head.x-BLOCK_SIZE, self.head.y),
+                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
+        
+        self.score = 0
+        self.food = None
+        self._place_food()
+        self.frame_iteration = 0
+        
+    def _place_food(self):
+        x = random.randint(0, (self.w-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE 
+        y = random.randint(0, (self.h-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
+        self.food = Point(x, y)
+        if self.food in self.snake:
+            self._place_food()
+        
+    def play_step(self, action):
+        self.frame_iteration += 1
+        # 1. collect user input
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        
+        # 2. move
+        self._move(action) # update the head selon l'action
+        self.snake.insert(0, self.head)
+        
+        # 3. check if game over
+        game_over = False
+        reward = 0
+        # fin de partie s'il y a une collision ou bien si on arrive par a manger la pomme pendant un long temps (selon la longueur du snake)
+        if self._is_collision() or self.frame_iteration > 100*len(self.snake):
+            game_over = True
+            reward = -10
+            return reward, game_over, self.score
+            
+        # 4. place new food or just move
+        if self.head == self.food:
+            self.score += 1
+            reward = 10
+            self._place_food()
+        else:
+            self.snake.pop()
+        
+        # 5. update ui and clock
+        self._update_ui()
+        self.clock.tick(SPEED)
+        # 6. return game over and score
+        return reward, game_over, self.score
+    
+    def _is_collision(self, pt=None): # pt c'est pour pouvoir calculer le danger qui existe dans state
+        if pt is None : # pour pouvoir appeler _is_collision sans parametres
+            pt = self.head
+        # hits boundary
+        if pt.x > self.w - BLOCK_SIZE or pt.x < 0 or pt.y > self.h - BLOCK_SIZE or pt.y < 0:
+            return True
+        # hits itself
+        if pt in self.snake[1:]:
+            return True
+         
+        return False
+        
+    def _update_ui(self):
+        self.display.fill(BLACK)
+        
+        for pt in self.snake:
+            pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
+            pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
+            
+        pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
+        
+        text = font.render("Score: " + str(self.score), True, WHITE)
+        self.display.blit(text, [0, 0])
+        pygame.display.flip()
+        
+    def _move(self, action):
+        # Extraire la direction actuelle du serpont :
+        dir_Possible = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
+        idx_dir_actuelle = dir_Possible(self.direction)
+
+        # Faire un move selon l'action :
+        ## si action = [1, 0, 0] => rester dans la meme direction
+        if np.arry_equal(action, [1, 0, 0]):
+            new_dir = dir_Possible[idx_dir_actuelle]
+        ## si action = [0, 1, 0] => tourner a droite 
+        elif np.array_equal(action, [0, 1, 0]):
+            if idx_dir_actuelle == 3:
+                new_dir = dir_Possible[0]
+            else :
+                new_dir = dir_Possible[idx_dir_actuelle+1]
+        ## si action = [0, 0, 1] => tourne a gauche
+        else :
+            if idx_dir_actuelle == 0:
+                new_dir = dir_Possible[3]
+            else :
+                new_dir = dir_Possible[idx_dir_actuelle-1]
+        # mettre a jour la nouvelle direction
+        self.direction = new_dir
+        # Deplacer le serpent :
+        x = self.head.x
+        y = self.head.y
+        if self.direction == Direction.RIGHT:
+            x += BLOCK_SIZE
+        elif self.direction == Direction.LEFT:
+            x -= BLOCK_SIZE
+        elif self.direction == Direction.DOWN:
+            y += BLOCK_SIZE
+        elif self.direction == Direction.UP:
+            y -= BLOCK_SIZE
+            
+        self.head = Point(x, y)
